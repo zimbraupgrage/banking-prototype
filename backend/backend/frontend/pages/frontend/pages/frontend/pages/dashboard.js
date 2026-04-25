7958b73fa0d40db233fb72ae504de7acb75b8944
zimbraupgrage <zimbraupgrage@gmail.com>
2026-04-26 01:13:35 +0530

    Add dashboard page
    Add dashboard page

---
+++ backend/backend/frontend/pages/frontend/pages/frontend/pages/dashboard.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+
+const API_URL = "http://localhost:5000";
+
+export default function Home() {
+  const router = useRouter();
+
+  const login = async () => {
+    await fetch(`${API_URL}/login`, {
+      method: "POST"
+    });
+
+    router.push("/dashboard");
+  };
+
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <button onClick={login}>
+        Login (Mock Banking App)
+      </button>
+    </div>
+  );
+}
